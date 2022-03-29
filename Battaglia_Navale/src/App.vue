4bfafbe491c11f69bea09fd73ea18d7deb074813
Mattia-Zanini <mattzanini2@gmail.com>
2022-03-29 17:20:13 +0200

    Added the grid and tried to delete the border unnecessary

--- Battaglia_Navale/src/App.vue
+++ Battaglia_Navale/src/App.vue
@@ -10,33 +10,36 @@ Game();
       <h1 class="title-h1">Battleship</h1>
     </div>
     <div class="row">
-      <div class="col-4 detail-game">
+      <div class="col-5 detail-game">
         <DetailGame />
       </div>
-      <div class="col-8"></div>
+      <div class="col-6 d-flex justify-content-end">
+        <Grid />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import DetailGame from "./components/VUE/DetailGame.vue";
+import Grid from "./components/VUE/Grid.vue";
 export default {
-  /*
   // Properties returned from data() becomes reactive state
   // and will be exposed on `this`.
   data() {
     return {
-      count: 0
+      row: 0,
+      col: 0
     }
   },
-
-  // Methods are functions that mutate state and trigger updates.
-  // They can be bound as event listeners in templates.
-  methods: {
-    increment() {
-      this.count++
-    }
-  },*/
+  /*
+    // Methods are functions that mutate state and trigger updates.
+    // They can be bound as event listeners in templates.
+    methods: {
+      increment() {
+        this.count++
+      }
+    },*/
 
   // Lifecycle hooks are called at different stages
   // of a component's lifecycle.
---
+++ Battaglia_Navale/src/components/VUE/Grid.vue
@@ -0,0 +1,26 @@
+<template>
+    <table class="grid">
+        <tbody>
+            <tr v-for="row in 11" :class="{ 'no-border': (row == 1) }">
+                <td v-for="col in 11" :class="{ 'no-border': (col == 1) }"></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<!--
+<tbody>
+    <tr v-for="row in grid">
+        <td v-for="cell in row" @click="cellClicked(cell)" :class="cell.class">
+            <span v-if="cell.ship">
+                <i class="fas fa-ship"></i>
+            </span>
+            <span v-if="cell.miss">
+                <i class="fas fa-times"></i>
+            </span>
+            <span v-if="cell.hit">
+                <i class="fas fa-bomb"></i>
+            </span>
+        </td>
+    </tr>
+</tbody>
+-->
